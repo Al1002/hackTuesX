e6b442fe631318587f17d382cd549fad36aafa37
BvHand487 <boris.handjiev@gmail.com>
2024-03-16 01:23:23 +0200

    fishes 2

--- godot_files/FishHandler.gd
+++ godot_files/FishHandler.gd
@@ -5,6 +5,7 @@ var plane_mesh = QuadMesh.new()
 @export var scene : PackedScene
 @export var spawn_dist : float
 @export var group_dist : float
+@export var spawn_chance : float
 var player
 
 var fish_tex = load("res://assets/fauna/tropical_fish.png")
@@ -34,7 +35,6 @@ func _ready():
 	player = get_node("/root/Main/Player_character")
 
 func _process(delta):
-	if Input.is_action_just_pressed("spawn_fish"):
+	if randf_range(0, 1) < spawn_chance or Input.is_action_just_pressed("spawn_fish"):
 		var rand_dir = Vector3(randf_range(-1, 1), randf_range(-1, 1), randf_range(-1, 1)).normalized() * spawn_dist
 		spawn_fish_group(player.global_transform.origin + rand_dir, randi_range(20, 30))
-	
